2d1b0ce2baaf3c3737d7c5a93dc225397de32184
ManojSilag <40380004+ManojSilag@users.noreply.github.com>
2019-01-09 13:55:31 +0530

    Adding files

---
+++ app.js
@@ -0,0 +1,35 @@
+//Init Githhub
+const github = new Github;
+
+//Init UI
+const ui = new UI;
+
+//search input
+const searchUser = document.getElementById('searchUser');
+
+//Search input event listner
+searchUser.addEventListener('keyup', (e) =>{
+    //Get input text
+    const userText =e.target.value;
+    if(userText !== ''){
+        console.log(userText);
+
+        //Make http call
+        github.getUser(userText)
+        .then(data =>{
+            //console.log(data.profile);
+            
+            if(data.profile.message === 'Not Found'){
+            //Show alert
+            ui.showAlert('User not found', 'alert alert-danger')
+            } else{
+            //Show profile
+            ui.showProfile(data.profile);
+            ui.showRepos(data.repos)
+            }
+        })
+    } else {
+        //Clear profile
+        ui.clearProfile();
+    }
+})
